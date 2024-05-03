46bd4ed04f68ea28c8b57a378e42f8e52e3f31cb
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-03 20:44:18 +0000

    "implementation for hailstone challenge"

--- src/01_04/hailstone_challenge.c
+++ src/01_04/hailstone_challenge.c
@@ -3,10 +3,26 @@
 void hailstone(void)
 {
   int n=0;
+  int hailstoneLength = 1;
 
   printf("Enter a number greater that 0\n");
   scanf("%d", &n);
   printf("number is: %d" , n);
+
+  while(n>1)
+  {
+    if(n%2 == 0)
+    {
+      n = n/2;
+    }
+    else
+    {
+      n = (n*3) + 1;
+    }
+
+    printf(" %d ", n);
+    hailstoneLength++;
+  }
 }
 
 
