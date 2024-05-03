e15ff10f0870adccb693103f7d1bfe195895d586
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-03 20:38:24 +0000

    first draft of hailstone challenge

--- src/01_04/hailstone_challenge.c
+++ src/01_04/hailstone_challenge.c
@@ -1,1 +1,19 @@
-"This is a test"
+#include <stdio.h>
+
+void hailstone(void)
+{
+  int n=0;
+
+  printf("Enter a number greater that 0\n");
+  scanf("%d", &n);
+  printf("number is: %d" , n);
+}
+
+
+int main(void)
+{
+  hailstone();
+
+  printf("\n");
+  return 0;
+}
