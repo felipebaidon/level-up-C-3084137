86be3a1956ab60ec7a717f25041a7af8497016d5
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-07 21:55:33 +0000

    implemented challenge

--- src/01_08/shuffle_challenge.c
+++ src/01_08/shuffle_challenge.c
@@ -17,21 +17,44 @@ int main()
 		"A Change Is Gonna Come"
 	};
 	int frequency[PSIZE];
-	int x,r,count;
+	int recentSongs[PSIZE - 5];
+	int x,r,count, index;
+	int recentlyPlayed;
 
 	/* initialize */
 	srand( (unsigned)time(NULL) );
 	for(x=0;x<PSIZE;x++)
 		frequency[x] = 0;
 
+  for(x=0;x < (PSIZE -5); x++)
+	  recentSongs[x]=20;			  //initialize to invalid song
 
 	/* run through the playlist randomly */
 	puts("Playlist:");
 	count = 0;
+	index = 0;
 	while(count<100)
 	{
-		/* does r appear in recent[]? */
-		r = rand() % PSIZE;		/* random value */
+		recentlyPlayed = 1;
+
+		while(recentlyPlayed)
+		{
+			/* does r appear in recent[]? */
+			r = rand() % PSIZE; /* random value */
+			recentlyPlayed = 0;
+
+			for(x = 0; x < (PSIZE - 5); x++)
+			{
+				if (r == recentSongs[x])
+				{
+					recentlyPlayed = 1;
+				}
+			}
+		}
+
+		recentSongs[index++] = r;
+		index %= (PSIZE - 5);
+
 		printf("%3d: Now Playing '%s'\n",
 				count+1,
 				playlist[r]
@@ -43,6 +66,10 @@ int main()
 	puts("Song frequency:");
 	for(x=0;x<PSIZE;x++)
 		printf("%s: %d\n",playlist[x],frequency[x]);
+
+	puts("\nRecent Songs\n");
+	for (x = 0; x < (PSIZE -5); x++)
+		printf("%s \n", playlist[recentSongs[x]]);
 
 	return(0);
 }
