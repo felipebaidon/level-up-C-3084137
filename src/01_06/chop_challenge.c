5af40f90b6b3b58b43941616b3582b1f636d6e55
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-07 17:19:28 +0000

    finished string manipulation snippet

--- src/01_06/chop_challenge.c
+++ src/01_06/chop_challenge.c
@@ -3,14 +3,60 @@
 
 char *left(char *s,int len)
 {
+	char *newArray = (char *)malloc(sizeof(char)* (len+1));
+
+	for(int i  = 0; i < len ; i++)
+	{
+		*(newArray + i)= *(s+i);
+	}
+
+	newArray[len]  = '\0'; 
+
+	return newArray;
 }
 
 char *right(char *s,int len)
 {
+
+	char *newArray = (char *)malloc(sizeof(char)*(len+1));
+	int completeLen = 0;
+
+	if (newArray== NULL)
+	{
+		exit(1);
+	}
+
+	while(*s != '\0')
+	{
+		s++;
+		completeLen;
+	}
+
+	for(int i = 0; i < len; i++)
+	{
+		*(newArray + i) = s[completeLen - len + i];
+	}
+	newArray[len] = '\0';
+
+	return newArray;
 }
 
 char *mid(char *s,int offset, int len)
 {
+	char *newArray =  (char *)malloc(sizeof(char)*(len + 1));
+
+	if(newArray == NULL)
+	{
+		exit(1);
+	}
+
+	for(int i=0; i < len; i++)
+	{
+		*(newArray + i) = *(s + offset + i);
+	}
+
+	newArray[len] = '\0';
+	return newArray;
 }
 
 int main()
