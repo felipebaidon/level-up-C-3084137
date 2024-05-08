e32615cb0b1229c7c21875e0c1ef2bc2145d97b8
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-08 02:09:04 +0000

    tested King's movement, implemented debug functionality

--- src/01_09/kingWandererChallenge.c
+++ src/01_09/kingWandererChallenge.c
@@ -7,46 +7,108 @@
 #define rows 8
 #define columns 8
 
-#include<stdio.h>
+//#define DEBUG 
+#define RELEASE
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
 
 char chessboard[rows][columns];
 char horizontalAxis[columns + 1] = {' ', '1', '2', '3', '4', '5', '6', '7', '8'}; 
 char verticalAxis[rows]= {'1', '2', '3', '4', '5', '6', '7', '8'};
 char *King;
+enum movement{noMove, upperLeft, upperCenter, upperRight, left, right, lowerLeft, lowerCenter, lowerRight, NumOfMovements};
 
-int main(void)
+void drawGame(void)
 {
   int i = 0, j = 0;
   char grid;
-  King = &chessboard[3][4];
-  
-  printf( "Start: \n");
-  
-  for( j=0; j < columns + 1; j++)
-      printf(" %c ", horizontalAxis[j]);
+
+  printf("Start: \n");
+
+  for (j = 0; j < columns + 1; j++)
+    printf(" %c ", horizontalAxis[j]);
 
   puts("\n");
 
-  for(i= 0; i< rows; i++)
+  for (i = 0; i < rows; i++)
   {
     printf(" %c ", verticalAxis[i]);
 
-    for(j= 0; j< columns; j++)
+    for (j = 0; j < columns; j++)
     {
-      if(&chessboard[i][j]== King)
+      if (&chessboard[i][j] == King)
       {
-          grid = 'K';
+        grid = 'K';
       }
       else
       {
-          grid = '.';
+        grid = '.';
       }
       chessboard[i][j] = grid;
       printf(" %c ", chessboard[i][j]);
     }
     puts("\n");
   }
-  puts( "\n End \n");
+}
+
+
+void moveKing(void)
+{
+#ifdef RELEASE  
+  srand((unsigned)time(NULL));
+  enum movement direction = rand() % NumOfMovements;
+#endif
+#ifdef DEBUG
+  int direction = 0;
+  puts(" Waiting for user input:...\n");
+  scanf("%d", &direction);
+#endif
+
+  switch(direction)
+  {
+    case noMove:
+      break;
+    case upperLeft:
+      King = King - 9;
+      break;
+    case upperCenter:
+      King  = King - 8;
+      break;
+    case upperRight:
+      King = King - 7;
+      break;
+    case left:
+      King = King - 1;
+      break;
+    case right:
+      King = King + 1;
+      break;
+    case lowerLeft:
+      King = King + 7;
+      break;
+    case lowerCenter:
+      King = King + 8;
+      break;
+    case lowerRight:
+      King = King + 9;
+      break;
+  }
+}
+
+int main(void)
+{
+  King = &chessboard[3][4];
+
+  drawGame();
+  while(1)
+  {
+    moveKing();
+    puts("Movement performed!\n");
+    drawGame();
+  }
+
 
   return 0;
 }
