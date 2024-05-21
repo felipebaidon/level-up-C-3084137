da6ebafb60bddab64a5ee6bd4838e080b40de37c
felipebaidon <33642968+felipebaidon@users.noreply.github.com>
2024-05-21 00:34:22 +0000

    "Finished matrix rotation challenge"

--- src/01_10/matrixRotationChallenge.c
+++ src/01_10/matrixRotationChallenge.c
@@ -1,53 +1,72 @@
-#define MATRIX1ROWS 5
-#define MATRIX1COLUMNS 5
-
 #include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
 
-char Matrix_1[5][5];
-char NewMatrix_1[5][5];
+char Matrix[5][5];
 
-void fillInMatrix(void)
+struct grid{int rows; int columns;};
+struct grid Matrix_1grid = {5,5};
+struct grid Matrix_2grid = {8,3};
+struct grid Matrix_3grid = {4,6};
+
+void fillInMatrix(struct grid* grid_info)
 {
   char lower_bound = 'a';
   char upper_bound = 'z';
 
   srand((unsigned int)time(NULL));
 
-  for(int i= 0; i < MATRIX1ROWS ; i++)
+  for(int i= 0; i < grid_info ->rows ; i++)
   {
-    for(int j = 0; j < MATRIX1COLUMNS; j++)
+    for(int j = 0; j < grid_info -> columns; j++)
     {
-      Matrix_1[i][j] = rand() % (int)((upper_bound - lower_bound + 1)) + (int)lower_bound;
-      printf(" %c ", Matrix_1[i][j]);
+      Matrix[i][j] = rand() % (int)((upper_bound - lower_bound + 1)) + (int)lower_bound;
+      printf(" %c ", Matrix[i][j]);
     }
     puts("\n");
   }
 
 }
 
-void rotateMatrix(void)
+void rotateMatrix(struct grid* grid_info)
 {
-  for(int i = 0; i< MATRIX1COLUMNS ; i++)
+  char element;
+
+  for(int i = 0; i< grid_info->columns ; i++)
   {
-    for( int j = 0; j< MATRIX1ROWS; j++)
+    for( int j = 0; j< grid_info->rows; j++)
     {
-      NewMatrix_1[i][j] = Matrix_1[MATRIX1ROWS - 1 - j][i];
-      printf(" %c ", NewMatrix_1[i][j]);
+      element = Matrix[grid_info->rows - 1 - j][i];
+      printf(" %c ", element);
     }
     puts("\n");
   }
 }
 
 int main(void)
-{
+{ 
   printf(" Original Matrix \n");
-  fillInMatrix();
+  fillInMatrix(&Matrix_1grid);
   printf("\n");
 
-  (printf("Rotated Matrix\n"));
-  rotateMatrix();
+  printf("Rotated Matrix\n");
+  rotateMatrix(&Matrix_1grid);
+  printf("\n");
+
+  printf(" Original Matrix \n");
+  fillInMatrix(&Matrix_2grid);
+  printf("\n");
+
+  printf("Rotated Matrix\n");
+  rotateMatrix(&Matrix_2grid);
+  printf("\n");
+
+  printf(" Original Matrix \n");
+  fillInMatrix(&Matrix_3grid);
+  printf("\n");
+
+  printf("Rotated Matrix\n");
+  rotateMatrix(&Matrix_3grid);
   printf("\n");
 
   return 0;  
